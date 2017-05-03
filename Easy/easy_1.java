924eaa23e83d43741fd09c5da7e21556d4d1c98d
inquis1t0r <pi0tr3k9292@gmail.com>
2017-05-03 21:25:18 +0200

    changed naming convention

---
+++ Easy/easy_1.java
@@ -0,0 +1,28 @@
+/*
+Created by: Inquis1t0r
+*/
+
+import java.util.Scanner;
+
+class easy_1
+{
+    String name;
+    String age;
+    String redditUsername;
+
+
+    public static getName(String name, String Age, String RedditUsername)
+            {
+
+            }
+
+    public static void main( String[] args )
+            {
+
+
+            }
+
+
+
+
+}
