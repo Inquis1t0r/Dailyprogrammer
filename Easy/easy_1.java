f6b83d7887902bfe0d4982258ba29b2aad3f5869
inquis1t0r <pi0tr3k9292@gmail.com>
2017-05-04 21:56:09 +0200

    some progress on 1st challenge

--- Easy/easy_1.java
+++ Easy/easy_1.java
@@ -4,25 +4,28 @@ Created by: Inquis1t0r
 
 import java.util.Scanner;
 
-class easy_1
+public class easy_1
 {
-    String name;
-    String age;
-    String redditUsername;
 
 
-    public static getName(String name, String Age, String RedditUsername)
-            {
-
-            }
 
     public static void main( String[] args )
             {
+                String name;
+                String age;
+                String redditUsername;
 
+            Scanner myScanner = new Scanner(System.in);
 
+            System.out.println("What is your name?");
+            name = myScanner.nextLine();
+
+            System.out.println("How old are you?");
+            age = myScanner.nextInt().toString();
+
+            System.out.println("What is your reddit username?");
+            redditUsername = myScanner.nextLine();
+
+            System.out.println("your name is" + name +", you are" + age + "years old, and your username is" + redditUsername);
             }
-
-
-
-
 }
